de858a534f3e20ba56098e951e4163752102a5f0
Timmy <vexkiddy@Timmys-MBP-2.fritz.box>
2020-02-02 13:43:24 +0100

    inital commit

---
+++ src/components/Container.svelte
@@ -0,0 +1,16 @@
+<style lang="scss">
+     @import 'src/css/variables.scss';
+  
+  h1 {
+    text-align: center;
+    color: $color;
+  }
+
+  .container {
+      background-color: $main-background;
+  }
+</style>
+
+<div class='container'> 
+    <h1>Hello There!</h1>
+</div>
